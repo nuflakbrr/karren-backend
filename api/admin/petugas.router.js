ed57ca33174530598e817221a9eba3ea2d0df932
Naufal Akbar <kbrnugroho@gmail.com>
2022-04-09 06:21:40 +0700

    hotfix

--- api/admin/petugas.router.js
+++ api/admin/petugas.router.js
@@ -4,8 +4,8 @@ const { controllerAdd, controllerDelete, controllerGet, controllerGetId, control
 
 router.post("/", controllerAdd)
 router.get("/", checkToken, controllerGet)
-router.get("/id", checkToken, controllerGetId)
-router.patch("/", checkToken, controllerUpdate)
+router.get("/:id", checkToken, controllerGetId)
+router.put("/", checkToken, controllerUpdate)
 router.delete("/", checkToken, controllerDelete)
 router.post("/login", controllerLogin)
 
