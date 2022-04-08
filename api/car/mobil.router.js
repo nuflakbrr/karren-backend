62499f6c4728dc616e3539eec9379204db35f687
Naufal Akbar <kbrnugroho@gmail.com>
2022-04-08 08:13:07 +0700

    create car router

---
+++ api/car/mobil.router.js
@@ -0,0 +1,11 @@
+const router = require("express").Router()
+const { checkToken } = require("../../auth/tokenValidation")
+const { controllerAdd, controllerDelete, controllerGet, controllerGetId, controllerUpdate } = require("./mobil.controller")
+
+router.post('/', checkToken, controllerAdd)
+router.get('/', controllerGet)
+router.get('/id', controllerGetId)
+router.put('/', checkToken, controllerUpdate)
+router.delete('/', checkToken, controllerDelete)
+
+module.exports = router 
