e861d8f43b00865563b0937e67db27dc25868362
Naufal Akbar <kbrnugroho@gmail.com>
2022-04-09 08:24:43 +0700

    testing using local api

--- config/db.js
+++ config/db.js
@@ -2,16 +2,16 @@ const { createPool } = require('mysql')
 const mysql = require('mysql2')
 
 // LOCAL SQL
-// const db = createPool({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASS,
-//     database: process.env.MYSQL_DB,
-//     connectionLimit: 10
-// })
+const db = createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.MYSQL_DB,
+    connectionLimit: 10
+})
 
 // CLOUD SQL
-const db = mysql.createConnection(process.env.DB_CLOUD_PROD || process.env.DB_CLOUD_DEV)
-db.connect()
+// const db = mysql.createConnection(process.env.DB_CLOUD_PROD || process.env.DB_CLOUD_DEV)
+// db.connect()
 
 module.exports = db
