afe4c289a8bac6957a0e2bb1407961f150a837cb
Naufal Akbar <kbrnugroho@gmail.com>
2022-04-09 08:20:31 +0700

    update option sql

--- config/db.js
+++ config/db.js
@@ -1,14 +1,17 @@
+const { createPool } = require('mysql')
 const mysql = require('mysql2')
 
-// const db = createPool({
-//     host: process.env.DB_HOST_CLOUD,
-//     user: process.env.DB_USER_CLOUD,
-//     password: process.env.DB_PASS_CLOUD,
-//     database: process.env.DB_NAME_CLOUD,
-//     connectionLimit: 10
-// })
+// LOCAL SQL
+const db = createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.MYSQL_DB,
+    connectionLimit: 10
+})
 
-const db = mysql.createConnection(process.env.DB_CLOUD_PROD || process.env.DB_CLOUD_DEV)
-db.connect()
+// CLOUD SQL
+// const db = mysql.createConnection(process.env.DB_CLOUD_PROD || process.env.DB_CLOUD_DEV)
+// db.connect()
 
 module.exports = db
